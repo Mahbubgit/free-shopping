ca89038c66be4a262b6707467b20a1cb2375e0e3
Your Name <you@example.com>
2022-03-26 21:16:22 +0600

    answer of 2 questions added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import './Cart.css';
+import {deleteShoppingCart, removeFromLocalStorage} from '../../utilities/local-storage-db';
 
 const Cart = (props) => {
     const {cart} = props;
     // console.log(cart);
+    
+    // for(const product of cart){
+    //     console.log(product);
+    //     const cartId = product.id;
+    //     const productName = product.name;
+    // }
+
     return (
         <div>
-            <h3>Selected Items: {cart.length}</h3>
-            <button>Choose One For Me</button>
-            <button>Reset</button>
+            <h3>Selected Items</h3>
+            {/* <h3>Selected Items: {cart.length}</h3> */}
+                    
+            <div className='cart-item'>
+            
+                <img src={cart.img} alt="" />
+                    <h4>Name: {cart.name}</h4>
+                    <button onClick={() => removeFromLocalStorage()}>Remove</button>
+            </div>
+            <button >Choose One For Me</button>
+            
+            <button onClick={() => deleteShoppingCart()}>Reset</button>
         </div>
     );
 };
